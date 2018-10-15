4983e3f636e4c4c1fa1b6c0b39f75c3b43b5c054
Vitaly Bryukhovetsky <bruhovet@list.ru>
2018-10-15 13:21:53 +0300

    Правит ошибки в подключении normalize.scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,7 @@ var server = require("browser-sync").create();
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-    }))
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
